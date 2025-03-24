4f6294d2f602f8c2426f557a4c2940383a8c0269
BAGATHE <lolemadaly@gmail.com>
2025-03-24 15:48:04 +0300

    bug fix

--- src/main/java/com/itu/evaluation/controllers/admin/DashboardAdminController.java
+++ src/main/java/com/itu/evaluation/controllers/admin/DashboardAdminController.java
@@ -47,7 +47,7 @@ public DashboardAdminController(final DashboardService dashboardService) {
                 kpis.setTotalOffers((int) datas.get("totalOffers"));
                 kpis.setTotalInvoices((int)datas.get("totalInvoices"));
                 kpis.setTotalInvoiceAmount(Double.parseDouble((String)datas.get("totalInvoiceAmount")));
-                kpis.setTotalPayment((int)datas.get("totalPayment"));
+                kpis.setTotalPayment(Integer.parseInt(datas.get("totalPayment").toString()));
                 model.addAttribute("kpis", kpis);
 
                 /*ssource payment**/
