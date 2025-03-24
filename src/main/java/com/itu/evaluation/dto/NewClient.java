c11451052a3ea808a00bdd5e101074e6c7bb3bad
BAGATHE <lolemadaly@gmail.com>
2025-03-24 10:35:05 +0300

    line chart

---
+++ src/main/java/com/itu/evaluation/dto/NewClient.java
@@ -0,0 +1,16 @@
+package com.itu.evaluation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewClient {
+        private int client_number;
+        private int month;
+        private int year;
+}
